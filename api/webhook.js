85afd0a209376c959dd77f724c3991bb85b33d84
frr3112 <frr3112@hotmail.com>
2026-04-26 23:12:37 +0800

    Add POST request handler to webhook

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,12 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.status(200).json({
+      message: "POST received",
+      body: req.body
+    });
+  }
 
+  return res.status(200).json({
+    message: "Send a POST request"
+  });
+}
